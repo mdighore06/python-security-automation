3f0921c6ef079e2112673bdbd91f8d740471ed4a
mdighore06 <dighoremayur18@gmail.com>
2026-08-16 19:26:12 +0530

    Update test_port_scanner.py

--- tests/test_port_scanner.py
+++ tests/test_port_scanner.py
@@ -1,1 +1,9 @@
+import socket
+from port_scanner import grab_banner
 
+def test_grab_banner_timeout():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(0.01)
+    banner = grab_banner(sock)
+    assert banner == "No banner"
+    sock.close()
