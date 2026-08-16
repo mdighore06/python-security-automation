667ddfcf961c6331034b7073a7577f043b8821c6
mdighore06 <dighoremayur18@gmail.com>
2026-08-16 19:28:14 +0530

    Update test_threat_detector.py

--- tests/test_threat_detector.py
+++ tests/test_threat_detector.py
@@ -1,1 +1,6 @@
+from ml.threat_detector import load_and_preprocess_data
 
+def test_load_and_preprocess_data():
+    X, y = load_and_preprocess_data()
+    assert not X.empty
+    assert len(X) == len(y)
